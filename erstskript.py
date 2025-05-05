550e38a2483b581da596e4c1420a7fbb04a0ca7c
emre.coskun@skyered.de <emre.coskun@skyered.de>
2025-05-05 11:32:59 +0200

    my python script

---
+++ erstskript.py
@@ -0,0 +1,40 @@
+name = "emre"
+print(name)
+zahl1 = 1.4
+zahl2 = 2.3
+zahlergebnis = zahl1 + zahl2 
+print(zahlergebnis)
+alter= float (input("gib das alter ein"))
+
+if alter<18:
+  print("du bist zu jung")
+
+elif alter>=18:
+  print ("dein alter passt")
+else:
+  print ("kein alter")           
+for i in range(1,6):
+  print(i)
+fruechte=["bannane","apfel","erdbeer","Kiwi","Bierne", "bannane","apfel","erdbeer","Kiwi","Bierne"] 
+print(fruechte[0]) 
+
+
+for frucht in fruechte:
+  print("-"+frucht+"-")
+for i in range(0,11, 2):
+  print(i) 
+  
+for frucht in fruechte:
+  if frucht=="erdbeer":
+      continue
+  else:
+      print( frucht)
+
+i=0;
+
+while len(fruechte)<=i:
+  print(fruechte[i])  
+  i+=1      
+
+enter = input("gib etwas ein")
+print (enter)                      
